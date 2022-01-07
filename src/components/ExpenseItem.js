4a380778d39b8bac05b9644e3f6c68d09542e43a
thulasigovardhan <thulasigovardhan@gmail.com>
2022-01-07 17:54:31 -0500

    Changed the date format to use in-built methods

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(expenseprops) {
+    const month = expenseprops.date.toLocaleString('en-US', {month: 'long'});
+    const day = expenseprops.date.toLocaleString('en-US', {day: '2-digit'});
+    const year = expenseprops.date.getFullYear();
 
     return (<div className='expense-item'>
-        <div>{expenseprops.date.toISOString()}</div>
+        <div>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+        </div>
         <div className='expense-item__description'>
         <h2>{expenseprops.title}</h2>
         <div className='expense-item__price'>${expenseprops.amount}</div>
