1d6f29c3a5f753c9c33620c947b57ee63948dda1
thulasigovardhan <thulasigovardhan@gmail.com>
2022-01-10 16:46:56 -0500

    first assignment of course: second solution

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -5,9 +5,25 @@ function Expenses(expensesprops) {
     return (
         <div className="expenses">
             <ExpenseItem 
-                date={expensesprops.date}
-                title={expensesprops.title}
-                amount={expensesprops.amount} /> 
+                title={expensesprops.items[0].title} 
+                amount={expensesprops.items[0].amount} 
+                date={expensesprops.items[0].date}
+            />
+            <ExpenseItem 
+                title={expensesprops.items[1].title} 
+                amount={expensesprops.items[1].amount} 
+                date={expensesprops.items[1].date}
+            />
+            <ExpenseItem
+                title={expensesprops.items[2].title} 
+                amount={expensesprops.items[2].amount} 
+                date={expensesprops.items[2].date}
+            />
+            <ExpenseItem 
+                title={expensesprops.items[3].title} 
+                amount={expensesprops.items[3].amount} 
+                date={expensesprops.items[3].date}
+            /> 
         </div>
     )
 };
