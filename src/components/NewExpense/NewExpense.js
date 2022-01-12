677c4d8be52f5414990eeb238b67e8af1e620ea3
thulasigovardhan <thulasigovardhan@gmail.com>
2022-01-12 17:33:44 -0500

    Demonstrating the child-parent component communication via defined event handlers and props

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,18 @@
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+    const saveExpenseDataHandler = (enteredexpenseData) => {
+        const expenseData = {
+            ...enteredexpenseData,
+            id: Math.random().toString()
+        };
+        props.onAddExpense(expenseData);
+        console.log(expenseData);
+    };
     return (
         <div className="new-expense">
-            <ExpenseForm/>
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
         </div>
     );
 
