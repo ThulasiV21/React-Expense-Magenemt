b4c78cada1327c115615f29a52eab538daa4a402
thulasigovardhan <thulasigovardhan@gmail.com>
2022-01-12 15:28:41 -0500

    Demonstrating use of events

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,7 +2,10 @@ import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-const ExpenseItem = (expenseprops) => {  
+const ExpenseItem = (expenseprops) => {
+    const clickHandler = () =>{
+        console.log('Clicked!');
+    } 
 
     return (
     <Card className='expense-item'>
@@ -11,6 +14,7 @@ const ExpenseItem = (expenseprops) => {
         <h2>{expenseprops.title}</h2>
         <div className='expense-item__price'>${expenseprops.amount}</div>
         </div>
+        <button onClick={clickHandler}>Change Title</button>
     </Card>);
 }
 export default ExpenseItem;
