4725a676282f46d36ae47918939ef064338e387f
thulasigovardhan <thulasigovardhan@gmail.com>
2022-01-15 15:47:30 -0500

    Demonstrating use of stateful lists, rendering those lists with keys

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,39 +5,26 @@ import Card from "../UI/Card";
 import './Expenses.css';
 
 const Expenses = (expensesprops) => {
-    const [filteredYear, setfilteredYear] = useState('2020');
+    const [filteredYear, setFilteredYear] = useState('2020');
     
     const expenseItems = expensesprops.expenseitems;
-    
+
     const filterChangeHandler = (selectedYear) => {
-        setfilteredYear(selectedYear);
+        setFilteredYear(selectedYear);
     };
 
     return (
         <Card className="expenses">
-        <ExpensesFilter selected={filteredYear} onChangeOfFilter={filterChangeHandler}/>
-            <ExpenseItem 
-                title={expenseItems[0].title} 
-                amount={expenseItems[0].amount} 
-                date={expenseItems[0].date}
-            />
-            <ExpenseItem 
-                title={expenseItems[1].title} 
-                amount={expenseItems[1].amount} 
-                date={expenseItems[1].date}
-            />
+            <ExpensesFilter selected={filteredYear} onChangeOfFilter={filterChangeHandler}/>
+            {expensesprops.expenseitems.map((expense) => (
             <ExpenseItem
-                title={expenseItems[2].title} 
-                amount={expenseItems[2].amount} 
-                date={expenseItems[2].date}
-            />
-            <ExpenseItem 
-                title={expenseItems[3].title} 
-                amount={expenseItems[3].amount} 
-                date={expenseItems[3].date}
-            /> 
+            key={expense.id} 
+            title={expense.title} 
+            amount={expense.amount} 
+            date={expense.date}
+            />))}
         </Card>
-    )
+    );
 };
 
 
