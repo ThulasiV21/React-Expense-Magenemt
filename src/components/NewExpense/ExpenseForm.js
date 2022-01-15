d66e7e6d60e89534cfd4879ec30f8a96f0aaec66
thulasigovardhan <thulasigovardhan@gmail.com>
2022-01-15 18:34:39 -0500

    Assignment: My solution to hiding and showing section to add expenses

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,9 +2,12 @@ import { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = (props) => {
+
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
+
+    const [showExpenseSection, setShowExpenseSection] = useState(false);
 
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
@@ -33,26 +36,49 @@ const ExpenseForm = (props) => {
         setEnteredDate('');
     };
 
+    const showExpenseSectionHandler = (event) => {
+        setShowExpenseSection(true);
+    }
+
+    const unShowExpenseSectionHandler = (event) => {
+        setShowExpenseSection(false);
+    }
+
+    const renderExpenseSection = () => {
+        if (showExpenseSection) {
+            return (<form onSubmit={submitHandler}>
+                <div className='new-expense__controls'>
+                    <div className='new-expense__control'>
+                        <label>Title</label>
+                        <input type='text' value={enteredTitle} onChange={titleChangeHandler}/>
+                    </div>
+                    <div className='new-expense__control'>
+                        <label>Amount</label>
+                        <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/>
+                    </div>
+                    <div className='new-expense__control'>
+                        <label>Date</label>
+                        <input type='date' min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler}/>
+                    </div>
+                </div>
+                <div className='new-expense__actions'>
+                    <button type='submit'>Add Expense</button>
+                    <button onClick={unShowExpenseSectionHandler} type='button'>Cancel</button>
+                </div>
+            </form>)
+        }
+    }
+    
+
+    let screen = 
+    (<div className='new-expense__actions'>
+            <button onClick={showExpenseSectionHandler} type='submit'>Add Expense</button>
+             {renderExpenseSection()}
+        </div>);
+
+
         return (
-        <form onSubmit={submitHandler}>
-            <div className='new-expense__controls'>
-                <div className='new-expense__control'>
-                    <label>Title</label>
-                    <input type='text' value={enteredTitle} onChange={titleChangeHandler}/>
-                </div>
-                <div className='new-expense__control'>
-                    <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/>
-                </div>
-                <div className='new-expense__control'>
-                    <label>Date</label>
-                    <input type='date' min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler}/>
-                </div>
-            </div>
-            <div className='new-expense__actions'>
-                <button type='submit'>Add Expense</button>
-            </div>
-        </form>
+            <div>{screen}</div>     
     )
 };
 
