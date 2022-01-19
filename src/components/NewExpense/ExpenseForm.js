645e44e2ec9ca3e9606b958daaf932c35892ac45
thulasigovardhan <thulasigovardhan@gmail.com>
2022-01-19 15:54:47 -0500

    Fixed the issue where amount to be saved as number instead of string

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -23,7 +23,7 @@ const ExpenseForm = (props) => {
 
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
+            amount: +enteredAmount,
             date: new Date(enteredDate)
         };
         // Calling the parent's defined event handler and passing args or in this case
